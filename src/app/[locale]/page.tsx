9df26f1764c305aae317ca12f9426e3bfebe6955
J-Romulo <joseromulo.10@hotmail.com>
2026-02-21 21:36:01 +1300

    refactor: remove outdated comments and improve code clarity in page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,19 +1,5 @@
 import { Homepage } from './homepage';
 
-//Errors: "All-in-on" solutuon
-// "Prove compliance on day one" section button, hover background transparent and button icons not showing on hover
-// "Why segura" section orange button on hover transparent
-// NO FAQ section in portuguese
-// "e nós somos certeza" badges section
-// "All-In_One " overview
-// Description of gartner report different depending on language
-// In some mobile devices the dropdown in the nav menu cuts the title
-// Experienced intermittently the menu dropdown not closing and staying ove rother menu dropdowns that I tried to open
-// Accessibility concerns, language switch not being focusable by keyboard, texts are not read by screen readers, faq is entirely not readable
-
-// Fix icons, create components for them or use from lucide
-// Responsividade
-// Acessibilidade
 export default function Home() {
   return <Homepage />;
 }
